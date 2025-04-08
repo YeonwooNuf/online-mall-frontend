3388b2f5e007329d41ef66d7b7cd25ea84df0168
장연우 <jangyw0927@naver.com>
2025-04-08 17:01:15 +0900

    // ProductCard 구조 생성

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-const ProductCard = () => {
+// const ProductCard = (props) => {
+//     return <span>{props.name}/{props.price}</span>;
+// }
 
-    return(
-        <span>
-            ProductCard
-        </span>
+{/* key:value(전달값) 동일하면 : 생략 */ }
+const ProductCard = ({name, price}) => {
+    return (
+        <div className="product-card">
+            <h2>{name}</h2>
+            <p>Price : ${price}</p>
+        </div>
     );
-}
+};
 
 export default ProductCard;
