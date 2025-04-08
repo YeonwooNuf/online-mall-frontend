a425e165b2c95bc578b13fa67e9f7ef2bc59a395
장연우 <jangyw0927@naver.com>
2025-04-08 15:15:26 +0900

    // 컴포넌트 요소 기본 프레임 생성

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            Header Area
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/ProductCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductCard = () => {
+
+    return(
+        <span>
+            ProductCard
+        </span>
+    );
+}
+
+export default ProductCard;
