04af34f64c1bad2f31b1f00751dfd4c909015b4b
장연우 <jangyw0927@naver.com>
2025-04-09 10:24:05 +0900

    // Route 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 // import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Home />
+      {/* <Home /> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
